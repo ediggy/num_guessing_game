8a3731ee04f6fb927f1777387ad268d8ec967d4a
Elizabeth Sloan <esloan94@gmail.com>
2022-05-26 14:46:01 -0500

    Third Commit. Correct Indentation

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -5,31 +5,31 @@ public class HiLo {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String playAgain = "";
-		
+
 		do {
-			
-		
-		
-		// Create a random number for the user to guess
-		int targetNum = (int)(Math.random() * 100 + 1);
-		//System.out.println(targetNum);
-		
-		int guess = 0;
-		int numGuess = 0;
-		while (guess != targetNum) {
-			System.out.print("Guess a number between 1 and 100: ");
-			guess = scan.nextInt();
-			if (guess < targetNum) {
-				System.out.println(guess + " is too low. Try again.");
-			} else if (guess > targetNum){
-				System.out.println(guess + " is too high. Try again.");
+
+
+
+			// Create a random number for the user to guess
+			int targetNum = (int)(Math.random() * 100 + 1);
+			//System.out.println(targetNum);
+
+			int guess = 0;
+			int numGuess = 0;
+			while (guess != targetNum) {
+				System.out.print("Guess a number between 1 and 100: ");
+				guess = scan.nextInt();
+				if (guess < targetNum) {
+					System.out.println(guess + " is too low. Try again.");
+				} else if (guess > targetNum){
+					System.out.println(guess + " is too high. Try again.");
+				}
+				numGuess ++;
 			}
-			numGuess ++;
-		}
-		
-		System.out.println("Good Job! It took you " + numGuess + " trys!");
-		System.out.println("Would you like to play again? y/n");
-		playAgain = scan.next();
+
+			System.out.println("Good Job! It took you " + numGuess + " trys!");
+			System.out.println("Would you like to play again? y/n");
+			playAgain = scan.next();
 
 		} while (playAgain.equalsIgnoreCase("y"));
 	}
