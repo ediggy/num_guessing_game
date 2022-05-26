f12375881adb128be48e3bb0a434518cdf3fd30b
Elizabeth Sloan <esloan94@gmail.com>
2022-05-26 16:40:04 -0500

    Final Commit

--- HiLo/src/HiLo.java
+++ HiLo/src/HiLo.java
@@ -1,24 +1,40 @@
+/*
+ * Creator: Elizabeth Sloan
+ * Credit: Learn Java the Easy Way (Bryson Payne)
+ * 
+ * This is a simple number guessing game. It was created by following a tutorial.
+ * I have kept most of the content from the book, but I have modified the layout
+ * slightly.
+ */
+
 import java.util.Scanner;
 
 public class HiLo {
 
+	public static String playAgain = "";
+	public static int min = 1;
+	public static int max = 100;
+	public static int guess = 0;
+	public static int numGuess = 0;
+
+	public static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String playAgain = "";
 
 		do {
+			int targetNum = (int)(Math.random() * max + min);
+			System.out.println(targetNum);
 
-
-
-			// Create a random number for the user to guess
-			int targetNum = (int)(Math.random() * 100 + 1);
-			//System.out.println(targetNum);
-
-			int guess = 0;
-			int numGuess = 0;
 			while (guess != targetNum) {
-				System.out.print("Guess a number between 1 and 100: ");
+				System.out.print("Guess a number between " + min + " and " + max + ": ");
+				
+				while (!scan.hasNextInt()) {
+					System.out.println("Please enter a whole number.");
+					scan.next();
+				}
+				
 				guess = scan.nextInt();
+				
 				if (guess < targetNum) {
 					System.out.println(guess + " is too low. Try again.");
 				} else if (guess > targetNum){
@@ -28,10 +44,14 @@ public class HiLo {
 			}
 
 			System.out.println("Good Job! It took you " + numGuess + " trys!");
+			numGuess = 0;
 			System.out.println("Would you like to play again? y/n");
 			playAgain = scan.next();
 
 		} while (playAgain.equalsIgnoreCase("y"));
+		
+		System.out.println("Thanks for playing! Goodbye.");
+		scan.close();
 	}
-
 }
+
